0907f799705d3dd619207fcb2adb553a4661b58c
Max Harpunsky <>
2023-10-04 17:13:10 +0300

    fastapi server for gradio

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,26 @@
-FROM onerahmet/openai-whisper-asr-webservice:v1.1.1
+FROM python:3.11-slim
 
-ENV ASR_MODEL=large-v2
-# ENV ASR_ENGINE=openai_whisper
-ENV ASR_ENGINE=faster_whisper
+ENV POETRY_VENV=/app/.venv
+
+RUN export DEBIAN_FRONTEND=noninteractive \
+    && apt-get -qq update \
+    && apt-get -qq install --no-install-recommends \
+    ffmpeg \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN python3 -m venv $POETRY_VENV \
+    && $POETRY_VENV/bin/pip install -U pip setuptools \
+    && $POETRY_VENV/bin/pip install poetry==1.6.1
+
+ENV PATH="${PATH}:${POETRY_VENV}/bin"
+
+WORKDIR /app
+
+COPY . /app
+
+RUN poetry config virtualenvs.in-project true
+RUN poetry install
+
+EXPOSE 8080
+
+CMD ["uvicorn", "app.webservice:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ app/webservice.py
@@ -0,0 +1,51 @@
+import os
+from fastapi import FastAPI, File, UploadFile, APIRouter
+from gradio_client import Client
+import logging
+import uuid
+
+API_URL = "http://34.67.101.82:7860/"
+VIDEO_DIRECTORY = "static"
+os.makedirs(VIDEO_DIRECTORY, exist_ok=True)
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger("whisper")
+
+app = FastAPI()
+router = APIRouter()
+client = Client(API_URL)
+
+def transcribe_audio(audio_path, task="transcribe", return_timestamps=False):
+    text, runtime = client.predict(
+        audio_path,
+        task,
+        return_timestamps,
+        api_name="/predict_1",
+    )
+    return text
+
+# @router.get("/")
+# async def home():
+#     return {"message": "Machine Learning service"}
+
+@router.post("/asr")
+async def asr(audio_file: UploadFile = File(...)):
+    filepath = os.path.basename(audio_file.filename)
+    logger.info(filepath)
+
+    audio_path = f"{VIDEO_DIRECTORY}/{str(uuid.uuid4())}"
+    with open(audio_path, "wb+") as fp:
+        fp.write(audio_file.file.read())
+
+    try:
+        output_with_timestamps = transcribe_audio(audio_path, return_timestamps=False)
+        return output_with_timestamps
+    except Exception as e:
+        raise RuntimeError(f"Error: {e}") from e
+    finally:
+        os.remove(audio_path)
+
+app.include_router(router)
+
+# if __name__ == "__main__":
+#     uvicorn.run("app:app", reload=True, port=6000, host="0.0.0.0")
